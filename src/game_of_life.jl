f9c692b04407b357ae426942b0186884de16697e
sof202 <s.o.fletcher@exeter.ac.uk>
2025-06-20 12:30:49 +0100

    docs: add doctstring to exported functions
    Bad practice here as the setup_random_board() has a random element to
    it. But I realise that UnicodePlots doesn't care about this so neither
    do I for now.

--- src/game_of_life.jl
+++ src/game_of_life.jl
@@ -1,3 +1,4 @@
+"Project module that implements Conway's 'game of life' cellular automaton"
 module game_of_life
 
 using UnicodePlots: height
@@ -60,11 +61,59 @@ function plot_board(board::Matrix{Bool}, width::Int, height::Int)
     display(board_plot)
 end
 
+"""
+    setup_random_board(width, height)
+
+# Description
+
+Creates a Boolean matrix with random values of set width and height (for use 
+with [`play_game`](@ref)).
+
+# Examples
+
+```julia-repl
+julia> setup_random_board(5,5)
+5×5 Matrix{Bool}:
+ 0  1  0  1  1
+ 0  0  1  0  0
+ 0  1  1  1  0
+ 1  0  0  0  0
+ 1  0  1  0  1
+```
+
+# See also
+
+[`play_game`](@ref)
+"""
 function setup_random_board(width::Int, height::Int)
     board = rand(Bool, (height, width))
     return board
 end
 
+"""
+    create_glider(width,height)
+
+# Description
+
+Creates a Boolean matrix of set width and height that contains a glider in the
+bottom left (for use with [`play_game`](@ref)).
+
+# Examples
+
+```julia-repl
+julia> create_glider(5,5)
+5×5 Matrix{Bool}:
+ 1  0  0  0  0
+ 0  1  1  0  0
+ 1  1  0  0  0
+ 0  0  0  0  0
+ 0  0  0  0  0
+```
+
+# See also
+
+[`play_game`](@ref)
+"""
 function create_glider(width::Int, height::Int)
     if width < 3 || height < 3
         println("Board must be larger than 3x3")
@@ -77,6 +126,47 @@ function create_glider(width::Int, height::Int)
     return board
 end
 
+"""
+    play_game(board_generator; width, height, max_steps)
+
+# Description
+
+Simulate Conway's game of life with some intial state generator for a total of
+max_steps (early exits if all cells are dead as the state has reached a stable
+point). SIGINT (CTRL+C) can be sent to terminate the simulation early.
+
+# Arguments
+
+- board_generator : A function that accepts a width::Int and a height::Int and
+    returns a Boolean Matrix (Matrix{Bool}) representing the initial
+    configuration of the game state.
+    - A malformed function will throw an assertion error
+
+# Examples
+
+```julia-repl
+julia> play_game(setup_random_board)
+# Animation plays...
+
+Final iteration
+    ┌────────────────────┐
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄│
+    │▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄▄▄│
+    │▄▄▄▄▄▄▄    ▄▄▄▄▄▄▄▄▄│
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄│
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄│
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄│
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄│
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄    ▄▄▄│
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄    ▄▄▄│
+    │▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄│
+    └────────────────────┘
+```
+
+# See also
+
+[`setup_random_board`](@ref), [`create_glider`](@ref)
+"""
 function play_game(board_generator::Function;
     width::Int=20, height::Int=20, max_steps::Int=100)
     step = 0
